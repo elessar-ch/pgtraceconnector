5500cd609cbc7da2cb4ecb4cff561f5e9358ea4a
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2024-01-20 14:15:53 +0100

    Add initial go.mod

---
+++ config.go
@@ -0,0 +1,14 @@
+package exampleconnector
+
+import "fmt"
+
+type Config struct {
+    AttributeName string `mapstructure:"attribute_name"`
+}
+
+func (c *Config) Validate() error {
+    if c.AttributeName == "" {
+        return fmt.Errorf("attribute_name must not be empty")
+    }
+    return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gord02/exampleconnector
+
+go 1.21.6
